77667b7b862dffca0fb043042dc32f327d6069bf
Mike Morrow <15792417+MikeyMoMo@users.noreply.github.com>
2025-05-30 22:23:05 +0800

    Update Gradient_Line_with_tft_espi.ino

--- Gradient_Line_with_tft_espi.ino
+++ Gradient_Line_with_tft_espi.ino
@@ -2,7 +2,7 @@
 
 TFT_eSPI tft = TFT_eSPI();
 bool doShow = false;
-float pctDone;
+float pctBlend;
 uint16_t blendedColor;
 
 uint16_t alphaBlend(uint8_t alpha, uint16_t fgc, uint16_t bgc) {
@@ -29,9 +29,9 @@ void drawGradientLine(int x0, int y0, int x1, int y1, uint16_t colorStart, uint1
   if (doShow) Serial.printf("Steps: %i\r\n", steps);
   int x = x0, y = y0;
   for (int i = 0; i <= steps; i++) {
-    pctDone = (float)i / steps;
-    blendedColor = alphaBlend((uint8_t)(pctDone * 255), colorEnd, colorStart);
-    if (doShow) Serial.printf("x %i, y %i, pctDone %f\r\n", x, y, pctDone);
+    pctBlend = (float)i / steps;
+    blendedColor = alphaBlend((uint8_t)(pctBlend * 255), colorEnd, colorStart);
+    if (doShow) Serial.printf("x %i, y %i, pctBlend %f\r\n", x, y, pctBlend);
     tft.drawPixel(x, y, blendedColor);
     if ((x0 - x1) == 0) y1 > y0 ? y++ : y--;
     if ((y0 - y1) == 0) x1 > x0 ? x++ : x--;
@@ -42,37 +42,39 @@ void loop() {
   //Vertical lines
   Serial.println("Vertical part 1");
   for (int x = 0; x < tft.width() / 2; x++) {
-    if (x == 0)
-      doShow = true;
-    else
-      doShow = false;
-    drawGradientLine(x, tft.height() - 1, x, 0,
-                     //Bottom, Top
-                     TFT_BLUE, TFT_RED);
+    //    if (x == 0)
+    //      doShow = true;
+    //    else
+    //      doShow = false;
+    //                                          Bottom,   Top
+    drawGradientLine(x, tft.height() - 1, x, 0, TFT_BLUE, TFT_RED);
   }
-  delay(5000);
+
+  delay(2000);
+
   Serial.println("Vertical part 2");
   for (int x = tft.width() / 2; x < tft.width(); x++) {
-    if (x == tft.width() / 2)
-      doShow = true;
-    else
-      doShow = false;
-    drawGradientLine(x, 0, x, tft.height() - 1,
-                     //Top,    Bottom
-                     TFT_WHITE, TFT_GREEN);
+    //    if (x == tft.width() / 2)
+    //      doShow = true;
+    //    else
+    //      doShow = false;
+    //                                          Bottom,    Top
+    drawGradientLine(x, tft.height() - 1, x, 0, TFT_WHITE, TFT_GREEN);
   }
-  delay(10000);
+  delay(5000);
   tft.fillScreen(TFT_WHITE);
 
   // Horizontal lines
   for (int y = 0; y < tft.height() / 2 - 1 ; y++)
-    drawGradientLine(0, y, tft.width(), y,
-                     //Left,  Right
-                     TFT_RED, TFT_BLUE);
+    //                                     Left,    Right
+    drawGradientLine(0, y, tft.width(), y, TFT_RED, TFT_BLUE);
+
+  delay(2000);
+
   for (int y = tft.height() / 2; y < tft.height() - 1; y++)
-    drawGradientLine(0, y, tft.width(), y,
-                     //Left,    Right
-                     TFT_GREEN, TFT_WHITE);
-  delay(10000);
+    //                                     Left,      Right
+    drawGradientLine(0, y, tft.width(), y, TFT_GREEN, TFT_WHITE);
+
+  delay(5000);
   tft.fillScreen(TFT_WHITE);
 }
